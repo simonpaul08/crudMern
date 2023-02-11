3f1eeb0fd942c465cf71e02d5895f2f61bc1f7d2
Po <po@po.com>
2023-02-11 10:55:26 +0700

    update

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,6 @@
 const express = require('express');
-const genres = require('../routes/genres');
-const customers = require('../routes/customers');
-const movies = require('../routes/movies');
-const rentals = require('../routes/rentals');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
-const returns = require('../routes/returns');
 
 const tests = require('../routes/tests');
 const chapters = require('../routes/chapters');
@@ -14,13 +9,8 @@ const error = require('../middleware/error');
 
 module.exports = function(app) {
   app.use(express.json());
-  app.use('/api/genres', genres);
-  app.use('/api/customers', customers);
-  app.use('/api/movies', movies);
-  app.use('/api/rentals', rentals);
   app.use('/api/users', users);
   app.use('/api/auth', auth);
-  app.use('/api/returns', returns);
 
   app.use('/api/tests', tests);
   app.use('/api/chapters', chapters);
